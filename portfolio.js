4b6cf376fdc4d3d6186add8f6b0f8e245a6969c3
estiaspir <59113650+estiaspir@users.noreply.github.com>
2025-10-09 12:32:09 +0300

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -24,24 +24,37 @@ window.onload = function(){
 const mylinks = document.querySelectorAll('.mylink');
 const myliink = document.querySelector('.mylink');
 const info = document.querySelector('.info')
+const role = document.querySelector('.role')
+
 
 mylinks.forEach((link)=>{
     link.addEventListener('click', ()=>{
         mylinks.forEach((link)=>{
 
         link.classList.remove('current')
+        
         })
         link.classList.toggle('current')
         if(link.className === 'about mylink current'){
-            info.innerHTML = 'The proccess of castig plastic agreggates into cement'
+            info.innerHTML = 'The proccess of '
+            role.innerHTML = 'casting plastic agreggates into cement'
         }
 
         if(link.className === 'home mylink current'){
-            info.innerHTML = 'Each item is uniqe'
+            info.innerHTML = 'Each item '
+            role.innerHTML = 'Is uniqe'
+           
+
+        }
+
+        if(link.className === 'product mylink current'){
+            info.innerHTML = 'Get yours '
+            role.innerHTML = 'Today'
         }
 
         if(link.className === 'contact mylink current'){
-            info.innerHTML = 'Send your comments. Let us know what you think about plastic waste'
+            info.innerHTML = 'Send your comments. '
+            role.innerHTML = 'Let us know what you think about plastic waste'
         }
     })
     
